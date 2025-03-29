87aed7e45de5800a068f993646f30527bb7c1df3
ravigupta <rg187888@gmail.com>
2025-03-29 11:25:45 +0530

    configure redux toolkit store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store.jsx";
 
-const router = createBrowserRouter([
-  {path: '/', element: <App />}
-])
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);

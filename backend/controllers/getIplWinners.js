ab089788ea9e14f2c8c0259436007b8f8040a345
ravigupta <rg187888@gmail.com>
2025-04-23 17:46:11 +0530

    winners-teams

--- backend/controllers/getIplWinners.js
+++ backend/controllers/getIplWinners.js
@@ -1,1 +1,18 @@
+import instance from "../utils/axios.js";
+
+const winnersTeams = async (req, res) => {
+  try {
+    const { data } = await instance.get("/ipl-winners");
+    res.status(200).json({ message: "ipl winner teams data", data: data });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Server Internal Error :: Winner teams",
+        error: error.message,
+      });
+  }
+};
+
+export default winnersTeams;
 
